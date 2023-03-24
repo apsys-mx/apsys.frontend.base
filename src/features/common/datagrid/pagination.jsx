a4717b1930f91e63358ff7e043d01736aaf3a9cc
Erika Moreno Miranda <erika.moreno@apsys.mx>
2023-03-24 10:34:43 -0600

    create pagination component

--- src/features/common/datagrid/pagination.jsx
+++ src/features/common/datagrid/pagination.jsx
@@ -1,7 +1,19 @@
-const { Box } = require('@mui/material')
+/**Mui resources */
+import { Box } from '@mui/material'
+import TablePagination from '@mui/material/TablePagination'
+import { useSelector } from 'react-redux'
 
 const Pagination = () => {
-	return <Box> paginador</Box>
+	return (
+		<Box>
+			<TablePagination
+				showFirstButton={true}
+				showLastButton={true}
+				labelRowsPerPage={'Filas por pagina'}
+				rowsPerPageOptions={[2, 10, 20, 30]}
+			/>
+		</Box>
+	)
 }
 
 export default Pagination
