117f34797ba4a888a982d6d59032acdd1b45fef8
Emmanuel Vega <emmanuel.vega@apsys.mx>
2023-03-24 16:24:55 -0600

    -> Add filters in API

--- src/features/home/home.endPoints.js
+++ src/features/home/home.endPoints.js
@@ -4,14 +4,14 @@ export const timesheetsEndPoint = timesheetsApi.injectEndpoints({
 	endpoints: (builder) => ({
 		getTimesheets: builder.query({
 			query(params) {
-				const { sorting, pagination } = params
+				const { sorting, pagination, filters } = params
 				var { sortBy, sortDirection } = sorting
 				var { pageNumber, pageSize } = pagination
 				pageNumber = pageNumber ? pageNumber : 0
 				pageSize = pageSize ? pageSize : 0
 				sortDirection = sortDirection && sortDirection.length > 0 ? sortDirection : 'desc'
 				sortBy = sortBy && sortBy.length > 0 ? sortBy : 'projectName'
-				var url = `Timesheets?sortBy=${sortBy}&sortDirection=${sortDirection}&pageNumber=${pageNumber}&pageSize=${pageSize}`
+				var url = `Timesheets?sortBy=${sortBy}&sortDirection=${sortDirection}&pageNumber=${pageNumber}&pageSize=${pageSize}&${filters}`
 				console.log(`URL::[${url}]`)
 				return {
 					url: url,
