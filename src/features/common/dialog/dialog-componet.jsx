d5f5f71912ce9d43547311fd4c70f06990289a11
lizbeth <lizbethfortanell@gmail.com>
2023-10-13 15:00:32 -0600

    Ajustes del componente de la modal

--- src/features/common/dialog/dialog-componet.jsx
+++ src/features/common/dialog/dialog-componet.jsx
@@ -1,30 +1,21 @@
-import {
-	Box,
-	Button,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogTitle,
-	Typography,
-} from '@mui/material'
 import React from 'react'
+import propTypes from 'prop-types'
+
+import { Box, Button, Dialog, DialogActions, DialogContent, Typography } from '@mui/material'
 import * as styles from './dialog-componet-styles'
 import { Close } from '@mui/icons-material'
 
 const DialogComponet = (props) => {
-	const {
-		open,
-		close,
-		onSave,
-		isLoading,
-		disabled,
-		primaryAction,
-		secondaryAction,
-		action,
-		content,
-		title,
-		icon
-	} = props
+	//Function props
+	const { close, primaryAction } = props
+	//Title Props
+	const { title, subtitle, primaryActionTitle, secondaryActionTitle } = props
+	//Bool props
+	const { open, disabled } = props
+	//Content Props
+	const { children, contentTitle } = props
+	//icon
+	const { icon } = props
 	return (
 		<Dialog
 			open={open}
@@ -33,36 +24,73 @@ const DialogComponet = (props) => {
 			aria-describedby='alert-dialog-description'
 		>
 			<Box sx={styles.root}>
-				<DialogTitle sx={styles.dialogTitle}>
+				<Box sx={styles.dialogTitle}>
 					{icon}
 					<div style={styles.titles}>
-						<Typography variant='subtitle2' sx={styles.dialogTitleText}>{title} </Typography>
-						<Typography variant='caption' sx={styles.dialogSubTitleText}>{title} </Typography>
+						<Typography variant='subtitle2' sx={styles.dialogTitleText}>
+							{title}{' '}
+						</Typography>
+						<Typography variant='caption' sx={styles.dialogSubTitleText}>
+							{subtitle}{' '}
+						</Typography>
 					</div>
-					<Close sx={styles.closeIcon} onClick={close}/>
-				</DialogTitle>
+					<Close sx={styles.closeIcon} onClick={close} />
+				</Box>
 				<DialogContent>
 					<Typography variant='body2' sx={styles.content}>
-						{content}
+						{contentTitle}
 					</Typography>
-					{action}
+					{children}
 				</DialogContent>
 				<DialogActions>
-					<Button variant='secondary' style={styles.cancelButton} onClick={close}>
-						{secondaryAction}
+					<Button
+						variant='secondary'
+						disabled={disabled}
+						style={styles.cancelButton}
+						onClick={close}
+					>
+						{secondaryActionTitle}
 					</Button>
 					<Button
 						variant='primary'
 						style={styles.acceptButton}
-						onClick={onSave}
-						disabled={isLoading || disabled}
+						onClick={primaryAction}
+						disabled={disabled}
 					>
-						{primaryAction}
+						{primaryActionTitle}
 					</Button>
 				</DialogActions>
 			</Box>
 		</Dialog>
 	)
 }
-
+DialogComponet.propTypes = {
+	open: propTypes.bool,
+	close: propTypes.func.isRequired,
+	primaryAction: propTypes.func.isRequired,
+	title: propTypes.string,
+	subtitle: propTypes.string,
+	primaryActionTitle: propTypes.string,
+	secondaryActionTitle: propTypes.string,
+	contentTitle: propTypes.string,
+	isLoading: propTypes.bool,
+	disabled: propTypes.bool,
+}
+DialogComponet.defaultProps = {
+	title: '',
+	subtitle: '',
+	primaryActionTitle: '',
+	secondaryActionTitle: '',
+	contentTitle: '',
+	disabled: false,
+	open: () => {
+		console.warn('open callback not defined')
+	},
+	close: () => {
+		console.warn('close callback not defined')
+	},
+	primaryAction: () => {
+		console.warn('primary Action callback not defined')
+	},
+}
 export default DialogComponet
