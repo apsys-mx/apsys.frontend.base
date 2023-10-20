9c8a46c228d1a4c2b22afb03f7811e3ac014b742
Carlos Almanza <carlopsd@gmail.com>
2023-10-20 16:08:36 -0600

    add alert stories

--- src/features/common/alert/alertComponent.stories.jsx
+++ src/features/common/alert/alertComponent.stories.jsx
@@ -6,14 +6,43 @@ export default {
     args: {  
         title: "Alert Title",
         description: "This is an Alert",
-        severityType: "warning",
-        open: false,
+        severityType: "info",
+        open: true,
     }
 }
 
 const Template = (args) => <AlertComponent {...args} />;
 
 export const InfoAlert = Template.bind({});
+InfoAlert.args ={
+    title: "Alert Title",
+        description: "This is an info alert",
+        severityType: "info",
+        open: true,
+}
 
+export const ErrorAlert = Template.bind({});
+ErrorAlert.args ={
+    title: "Alert Title",
+        description: "This is an error alert",
+        severityType: "error",
+        open: true,
+}
+
+export const WarningAlert = Template.bind({});
+WarningAlert.args ={
+    title: "Alert Title",
+        description: "This is an warnng alert",
+        severityType: "warning",
+        open: true,
+}
+
+export const SuccessAlert = Template.bind({});
+SuccessAlert.args ={
+    title: "Alert Title",
+        description: "This is an success alert",
+        severityType: "success",
+        open: true,
+}
 
 
