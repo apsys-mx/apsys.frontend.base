187a2ec8f3260b91b33afdac9aedcf9497f8a56d
Oscar M Sanchez <oscar.sanchez@apsys.mx>
2024-01-23 11:40:42 -0600

    Fix errorViewQuery

--- src/features/common/errorInfo/errorViewQuery.jsx
+++ src/features/common/errorInfo/errorViewQuery.jsx
@@ -3,20 +3,30 @@ import { Box } from '@mui/system'
 import React from 'react'
 import ErrorBadRequest from '../../../assets/img/ErrorBadRequest.png'
 import * as styles from './errorViewQuery.styles'
+import propTypes from 'prop-types'
+
+/**
+ * ErrorViewQuery component
+ */
 const ErrorViewQuery = ({ error }) => {
 	return (
 		<Box sx={styles.content}>
 			<Stack direction={'row'}>
 				<Box component={'img'} src={ErrorBadRequest} sx={styles.img} />
 				<details>
-					<div styles={styles.messageError}>
+					<Box sx={styles.messageError}>
 						Mensage: {'Error de consulta con la API'}
-					</div>
-					<div styles={styles.messageError}>Estatus: {error ? error.status : ''}</div>
-					<summary>Error: Ocurrio un error.</summary>
+					</Box>
+					<Box sx={styles.messageError}>Estatus: {error ? error.status : ''}
+					</Box>
+					<summary>Error: Ocurrió un error.</summary>
 				</details>
 			</Stack>
 		</Box>
 	)
 }
+ErrorViewQuery.propTypes = {
+	error: propTypes.object.isRequired
+}
+
 export default ErrorViewQuery
